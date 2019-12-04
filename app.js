7d6787dc246a148f8495713643db0a9c5f041dda
Jukka Aho <ahojukka5@gmail.com>
2019-12-04 19:12:00 +0700

    Add public html
    Give some response when accessing the root of site.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 const cors = require('cors');
 const testRouter = require('./controllers/testing');
 
+app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(cors());
 app.use('/api/testing', testRouter);
